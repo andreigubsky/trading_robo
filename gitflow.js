de5bc74081887774fc97c925625c90f92ae063dd
User <andrei.gubsky@gmail.com>
2023-07-22 12:51:16 +0300

    AG/Feature2_done

--- gitflow.js
+++ gitflow.js
@@ -1,20 +1,22 @@
+//Cloning git repo project
 git clone https://user@bitbucket.org/user/repo.git
 
 
-
+//Branches
 main->release->develop->feature_branch
 
+//Start work with git flow
 git flow feature start feature_branch
-
 git checkout -b feature_branch
 // Edit some code
 git commit -a -m "Add first draft of some feature"
-
 // Put in self repo
 git push origin feature_branch
-
+//
 git checkout develop
+//Merge new features on feature_branch witn develop branch
 git merge feature_branch
+//Finish work with git flow
 git flow feature finish 'feature_branch'
 
 
